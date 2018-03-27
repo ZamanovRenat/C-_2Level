c6d6db5d9ef1afc2f96065b242c81944731413d1
ZamanovRenat <zaman7797@gmail.com>
2018-03-27 18:44:04 +0300

    Решение домашнего задания 1 урока

---
+++ WindowsFormsApp1/WindowsFormsApp1/Game.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace MyGame
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+        // Свойства
+        // Ширина и высота игрового поля
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        static Game()
+        {
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Init(Form form)
+        {
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+            Load();
+
+            // Графическое устройство для вывода графики
+            Graphics g;
+            // предоставляет доступ к главному буферу графического контекста для текущего приложения
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics(); // Создаём объект - поверхность рисования и связываем его с формой
+                                       // Запоминаем размеры формы
+            Width = form.Width;
+            Height = form.Height;
+            // Связываем буфер в памяти с графическим объектом.
+            // для того, чтобы рисовать в буфере
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+        }
+
+        public static BaseObject[] _objs;
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            for (int i = 0; i < _objs.Length / 2; i++)
+                _objs[i] = new BaseObject(new Point(600, i * 20), new Point(-i, -i), new Size(10, 10));
+            for (int i = _objs.Length / 2; i < _objs.Length; i++)
+                _objs[i] = new BaseObject(new Point(600, i * 10), new Point(-i, 0), new Size(1, 1));
+        }
+
+        public static void Draw()
+        {
+            // Проверяем вывод графики
+            Buffer.Graphics.Clear(Color.Black);
+            //Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            //Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+
+            Buffer.Render();
+            Buffer.Graphics.Clear(Color.Black);
+
+            Image newImg = Image.FromFile("meteor-shower.png");
+            RectangleF rect = new RectangleF(0, 0, 1000, 1000);
+            Buffer.Graphics.DrawImage(newImg, rect);
+
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+    }
+}
